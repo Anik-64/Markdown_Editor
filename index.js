d8fa4bc88e86214fc47d1aa03acd463e9b353f9f
Anik Majumder <101924875+Anik-64@users.noreply.github.com>
2024-10-14 21:30:48 +0600

    Nothing special

--- index.js
+++ index.js
@@ -9,12 +9,10 @@ app.set('view engine', 'ejs');
 // public folder to store assets
 app.use(express.static('./public'));
 
-// routes for app
 app.get('/', (req, res) => {
     res.render('interface');
 });
 
-// listen on port 8000 (for localhost) or the port defined for heroku
 app.listen(process.env.PORT, () => {
     console.log(`Server is running ar port ${process.env.PORT}`);
 });
