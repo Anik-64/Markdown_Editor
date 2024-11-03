5d411352eb4c721beaa59d22cb9c57e0ac158a5d
Anik Majumder <101924875+Anik-64@users.noreply.github.com>
2024-11-03 19:46:24 +0600

    update

--- index.js
+++ index.js
@@ -6,23 +6,6 @@ require('dotenv').config();
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
-app.use(
-    helmet({
-        contentSecurityPolicy: {
-        directives: {
-            defaultSrc: ["'self'"],
-            scriptSrc: ["'self'", "https://vercel.live"],
-            scriptSrcElem: ["'self'", "https://vercel.live"], // For inline scripts in elements
-            connectSrc: ["'self'", "https://vercel.live"],    // For WebSocket or API calls
-            styleSrc: ["'self'", "'unsafe-inline'"],          // Adjust as needed for CSS styles
-            imgSrc: ["'self'", "data:"],                      // Allow images from same origin
-            frameSrc: ["'self'"],                             // Allow iframes from self if needed
-            // Add other directives as needed
-        },
-        },
-    })
-);
-
 
 // public folder to store assets
 app.use(express.static('./public'));
