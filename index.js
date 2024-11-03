42524a0d4d2d4c3883d3777c43ed948ece78ae1b
Anik Majumder <101924875+Anik-64@users.noreply.github.com>
2024-11-03 19:39:59 +0600

    update

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const helmet = require('helmet');
 const app = express();
 
 require('dotenv').config();
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self'", "https://vercel.live"],
+        // Add other sources as needed
+        },
+    })
+);
+
 
 // public folder to store assets
 app.use(express.static('./public'));
